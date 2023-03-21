b40119d42dc57b56441cd519755aece9bf53bf90
lotefi38 <lotefimohdeb@outlook.fr>
2023-03-21 13:53:56 +0100

    (feature/front-register) j'aimodifier html afin de faire le register/login

--- result.php
+++ result.php
@@ -1,13 +1,12 @@
 <?php
 require("database.php");
+require("verification.php");
 
 $array = [];
-function verif ($name, $min, $max, $message, $array){
-    if (strlen($name) < $min|| strlen($name) > $max) {
-        array_push($array,message);
-        return $array;
-    }
-    
-}
+$verif = new Verification();
+$verif->Email($_GET['email']);
+
+echo $verif -> getIndexError(0);
+
 $db = new Database();
 $db->connectDB();
